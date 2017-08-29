73faef0b4d1bfcae20a56074f15be62fba375b7b
Muhammad Ubaid Raza <mubaidr@gmail.com>
2017-08-29 22:28:16 +0500

    Add current time when clock not active

--- docs/index.js
+++ docs/index.js
@@ -4,23 +4,56 @@ Vue.config.productionTip = false
 var myApp = new Vue({
   el: '#app',
   data: {
-    title: 'Session',
-    time: '12:39',
     duration: {
       break: 15,
       session: 30
-    }
+    },
+    modes: ['Session', 'Break'],
+    activeModeId: 0,
+    isActive: true,
+    dateNow: '',
+    intervalID: null
   },
-  created () {},
+  created () {
+    setInterval(this.updateTime, 1000)
+  },
   mounted () {},
   watch: {},
-  computed: {},
+  computed: {
+    activeMode () {
+      return this.modes[this.activeModeId]
+    },
+    remainingTime () {
+
+    }
+  },
   methods: {
     setBreak (val) {
-      this.duration.break += val
+      if (this.duration.break + val > 0) {
+        this.duration.break += val
+      }
+      this.isActive = false
     },
     setSession (val) {
-      this.duration.session += val
+      if (this.duration.session + val > 0) {
+        this.duration.session += val
+      }
+      this.isActive = false
+    },
+    toggleClock () {
+      this.isActive = !this.isActive
+      this.startClock()
+    },
+    startClock () {
+
+    },
+    setNextMode () {
+      if (this.activeModeId++ > (this.modes.length - 1)) {
+        this.activeModeId = 0
+      }
+    },
+    updateTime () {
+      this.dateNow = new Date(new Date().getTime()).toLocaleTimeString()
     }
   }
 })
