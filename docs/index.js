4fe4ad94b8d2f5f1f3549fdc084f2ac2d4ad425f
Muhammad Ubaid Raza <mubaidr@gmail.com>
2017-08-29 14:47:00 +0500

    added base files

---
+++ docs/index.js
@@ -0,0 +1,18 @@
+// eslint-disable-next-line
+Vue.config.productionTip = false
+// eslint-disable-next-line
+var myApp = new Vue({
+  el: '#app',
+  data: {},
+  created () {},
+  mounted () {},
+  watch: {},
+  computed: {},
+  methods: {
+    /**
+     * @description Handles click event on digits, functions and operators
+     * @param {any} e Click event
+     */
+    handleClick (e) {}
+  }
+})
